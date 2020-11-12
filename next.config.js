ed8c63edc4cbc4a9756cee1b7fad584d76cbd77d
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2020-11-12 09:52:58 -0500

    clean up formatting

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_BUILD) {
     return {
-        basePath: '/next-blog-tutorial',
+      basePath: '/next-blog-tutorial',
     }
   }
 
