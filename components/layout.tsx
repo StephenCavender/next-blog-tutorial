4514c373aa7f4d5dc7956594840f3d31200d1693
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2020-11-12 10:52:31 -0500

    convert to TS

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,13 @@ import Link from 'next/link'
 const name = 'Stephen Cavender'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+    children,
+    home
+  }: {
+    children: React.ReactNode
+    home?: boolean
+  }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -39,7 +45,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <a>
                 <img
-                  src="images/profile.png"
+                  src="/images/profile.png"
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                   alt={name}
                 />
